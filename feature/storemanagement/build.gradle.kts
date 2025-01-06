0faee9176d46025a430889e0e6bc23f485422a09
Mino <k3000js@gmail.com>
2025-01-06 17:27:08 +0900

    [Feat] - Mapper / Model / Util 추가

--- feature/storemanagement/build.gradle.kts
+++ feature/storemanagement/build.gradle.kts
@@ -14,4 +14,9 @@ dependencies {
 
     implementation(libs.firebase.messaging.ktx)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.compose.coil)
+    implementation(libs.dialog.compose)
+    implementation(libs.dialog.time.compose)
+    implementation(libs.paging.compose)
 }
